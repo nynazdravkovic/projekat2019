eb9d17a8b3fc8ef695fee541e28cfda0fdbb9242
nynazdravkovic <nyna.zdravkovic@gmail.com>
2019-07-21 17:49:08 +0200

    Raste zauvek eksponencijalno

--- proba.py
+++ proba.py
@@ -29,8 +29,8 @@ ro22 = 0.
 
 op = 0.5
 oc = 0.5
-dp = 0.1
-dc = 0.1
+dp = 0.
+dc = 0.
 I = complex(0,1)
 ro = [ro00,ro01,ro02,ro10,ro11,ro12,ro20,ro21,ro22]
 k = np.full((4,9),(complex(0,0)))
@@ -39,14 +39,14 @@ f1 = Gamma01*ro[4] + Gamma02*ro[8] - 1.0*I*(0.5*op*ro[2] - 0.5*op*ro[6])
 f2 = -gamma01*ro[1] - 1.0*I*(0.5*oc*ro[2] - 0.5*op*ro[7] - ro[1]*(dc - dp))
 f3 = -gamma02*ro[2] - 1.0*I*(dp*ro[2] + 0.5*oc*ro[1] + 0.5*op*ro[0] - 0.5*op*ro[8])
 f4 = -gamma10*ro[3] - 1.0*I*(-0.5*oc*ro[6] + 0.5*op*ro[5] + ro[3]*(dc - dp))
-f5 = -Gamma01*ro[4] + Gamma12*ro[8] - 1.0*I*(0.5*oc*ro[5] - 0.5*oc*ro[7])
+f5 = 1 - ro[0] - ro[8]
 f6 = -gamma12*ro[5] - 1.0*I*(dp*ro[5] + 0.5*oc*ro[4] - 0.5*oc*ro[8] + 0.5*op*ro[3] + ro[5]*(dc - dp))
 f7 = -gamma20*ro[6] - 1.0*I*(-dp*ro[6] - 0.5*oc*ro[3] - 0.5*op*ro[0] + 0.5*op*ro[8])
 f8 = -gamma21*ro[7] - 1.0*I*(-dp*ro[7] - 0.5*oc*ro[4] + 0.5*oc*ro[8] - 0.5*op*ro[1] - ro[7]*(dc - dp))
 f9 = -Gamma02*ro[8] - Gamma12*ro[8] - 1.0*I*(-0.5*oc*ro[5] + 0.5*oc*ro[7] - 0.5*op*ro[2] + 0.5*op*ro[6])
 c= []
 t = 0.
-brojPonavljanja = 500
+brojPonavljanja = 50
 h = 0.001
 matricaGustineIm = np.zeros((9,brojPonavljanja))
 matricaGustineRe = np.zeros((9,brojPonavljanja))
@@ -54,8 +54,12 @@ for x in range(brojPonavljanja):
     F = [f1,f2,f3,f4,f5,f6,f7,f8,f9]
     for j in range(4):
         for i in range(9):
-            k[j][i] = F[i]
-            F[i] = ro[i]+h*k[j][i]/2
+            if(j!=3):
+                k[j][i] = h*F[i]
+                F[i] = ro[i]+k[j][i]/2
+            else:
+                k[j][i] = h*F[i]
+                F[i] = ro[i]+k[j][i]
     for i in range(9):
         ro[i] += (1/6)*(k[0][i]+2*k[1][i]+2*k[2][i]+k[3][i])
     for i in range(9):    
@@ -71,6 +75,7 @@ for i in range(9):
     plt.title("Realni deo")
     plt.xlabel("t")
     b = 'ro'+str(i)+'Re'
+    plt.ylabel(b)
     plt.savefig(b)
     plt.show()
 for i in range(9):
