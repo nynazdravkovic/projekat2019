e953c5f2cf26cbaaa49fa413b791d2778b6ea7ec
nynazdravkovic <nyna.zdravkovic@gmail.com>
2019-07-22 00:31:00 +0200

    Ponovo je dobro

--- rungekuta.py
+++ rungekuta.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jul 18 11:27:34 2019
-
 @author: nina
 """
 
@@ -10,8 +9,8 @@ Created on Thu Jul 18 11:27:34 2019
 #ro'10 = -ro10 - I*(d*ro10 - o*ro00/2 + o*ro11/2)
 #ro'11 = -ro11 - I*(-o*ro01/2 + o*ro10/2)
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-<<<<<<< HEAD
 Gamma02=1.
 Gamma12=1.
 Gamma01=1.
@@ -34,50 +33,52 @@ ro22 = 0.
 
 op = 0.5
 oc = 0.5
-dp = 0.1
-dc = 0.1
+dp = 0.
+dc = 0.
 I = complex(0,1)
-a = []
-b = []
-f = []
-a1 = []
-t = 0.
-h = 0.02
- 
-=======
-ro00 = 1
-ro01 = 0
-ro10 = 0
-ro11 = 0
-o = 0.5
-delte = []
-d = 0.1
-I = complex(0,1)
+
 matricaGustineIm = []
 matricaGustineRe = []
 a = []
 b = []
 c = []
+e = []
+f = []
 a1 = []
 t = 0.
 h = 0.02
->>>>>>> parent of cd9cfef... resava za sistem od 3 nivoa i 2 lasera
+
+f1 = Gamma01*ro11 + Gamma02*ro22 - 1.0*I*(0.5*op*ro02 - 0.5*op*ro20)
+f2 = -gamma01*ro01 - 1.0*I*(0.5*oc*ro02 - 0.5*op*ro21 - ro01*(dc - dp))
+f3 = -gamma02*ro02 - 1.0*I*(dp*ro02 + 0.5*oc*ro01 + 0.5*op*ro00 - 0.5*op*ro22)
+f4 = -gamma10*ro10 - 1.0*I*(-0.5*oc*ro20 + 0.5*op*ro12 + ro10*(dc - dp))
+f5 = -Gamma01*ro11 + Gamma12*ro22 - 1.0*I*(0.5*oc*ro12 - 0.5*oc*ro21)
+f6 = -gamma12*ro12 - 1.0*I*(dp*ro12 + 0.5*oc*ro11 - 0.5*oc*ro22 + 0.5*op*ro10 + ro12*(dc - dp))
+f7 = -gamma20*ro20 - 1.0*I*(-dp*ro20 - 0.5*oc*ro10 - 0.5*op*ro00 + 0.5*op*ro22)
+f8 = -gamma21*ro21 - 1.0*I*(-dp*ro21 - 0.5*oc*ro11 + 0.5*oc*ro22 - 0.5*op*ro01 - ro21*(dc - dp))
+f9 = -Gamma02*ro22 - Gamma12*ro22 - 1.0*I*(-0.5*oc*ro12 + 0.5*oc*ro21 - 0.5*op*ro02 + 0.5*op*ro20)
+
+ 
 while (t<4):
-    k11 = ro11 - I*(o*ro01/2. - o*ro10/2.)
-    k21 = -ro01 - I*(-d*ro01 + o*ro00/2. - o*ro11/2.)
-    k31 = -ro10 - I*(d*ro10 - o*ro00/2. + o*ro11/2.)
-    k41 = -ro11 - I*(-o*ro01/2. + o*ro10/2.)
+    k11 = Gamma01*ro11 + Gamma02*ro22 - 1.0*I*(0.5*op*ro02 - 0.5*op*ro20)
+    k21 = -gamma01*ro01 - 1.0*I*(0.5*oc*ro02 - 0.5*op*ro21 - ro01*(dc - dp))
+    k31 = -gamma02*ro02 - 1.0*I*(dp*ro02 + 0.5*oc*ro01 + 0.5*op*ro00 - 0.5*op*ro22)
+    k41 = -gamma10*ro10 - 1.0*I*(-0.5*oc*ro20 + 0.5*op*ro12 + ro10*(dc - dp)) 
+    k51 = -Gamma01*ro11 + Gamma12*ro22 - 1.0*I*(0.5*oc*ro12 - 0.5*oc*ro21)
+    k61 = -gamma12*ro12 - 1.0*I*(dp*ro12 + 0.5*oc*ro11 - 0.5*oc*ro22 + 0.5*op*ro10 + ro12*(dc - dp))
+    k71 = -gamma20*ro20 - 1.0*I*(-dp*ro20 - 0.5*oc*ro10 - 0.5*op*ro00 + 0.5*op*ro22)
+    k81 = -gamma21*ro21 - 1.0*I*(-dp*ro21 - 0.5*oc*ro11 + 0.5*oc*ro22 - 0.5*op*ro01 - ro21*(dc - dp))
+    k91 = -Gamma02*ro22 - Gamma12*ro22 - 1.0*I*(-0.5*oc*ro12 + 0.5*oc*ro21 - 0.5*op*ro02 + 0.5*op*ro20)
 #    ft2 = t + (h/2.)
-<<<<<<< HEAD
-    ro002 = ro[0]+(h/2.)*k11
-    ro012 = ro[1]+(h/2.)*k21
-    ro022 = ro[2]+(h/2.)*k31
-    ro102 = ro[3]+(h/2.)*k41
-    ro112 = ro[4]+(h/2.)*k51
-    ro122 = ro[5]+(h/2.)*k61
-    ro202 = ro[6]+(h/2.)*k71
-    ro212 = ro[7]+(h/2.)*k81
-    ro222 = ro[8]+(h/2.)*k91
+    ro002 = ro00+(h/2.)*k11
+    ro012 = ro01+(h/2.)*k21
+    ro022 = ro02+(h/2.)*k31
+    ro102 = ro10+(h/2.)*k41
+    ro112 = ro11+(h/2.)*k51
+    ro122 = ro12+(h/2.)*k61
+    ro202 = ro20+(h/2.)*k71
+    ro212 = ro21+(h/2.)*k81
+    ro222 = ro22+(h/2.)*k91
     k12 = Gamma01*ro112 + Gamma02*ro222 - 1.0*I*(0.5*op*ro022 - 0.5*op*ro202)
     k22 = -gamma01*ro012 - 1.0*I*(0.5*oc*ro022 - 0.5*op*ro212 - ro012*(dc - dp))
     k32 = -gamma02*ro022 - 1.0*I*(dp*ro022 + 0.5*oc*ro012 + 0.5*op*ro002 - 0.5*op*ro222)
@@ -88,7 +89,7 @@ while (t<4):
     k82 = -gamma21*ro212 - 1.0*I*(-dp*ro212 - 0.5*oc*ro112 + 0.5*oc*ro222 - 0.5*op*ro012 - ro212*(dc - dp))
     k92 = -Gamma02*ro222 - Gamma12*ro222 - 1.0*I*(-0.5*oc*ro122 + 0.5*oc*ro212 - 0.5*op*ro022 + 0.5*op*ro202)
 #    ft3 = t + (h/2.)
-    ro003 = ro[0]+(h/2.)*k12
+    ro003 = ro00+(h/2.)*k12
     ro013 = ro01+(h/2.)*k22
     ro023 = ro02+(h/2.)*k32
     ro103 = ro10+(h/2.)*k42
@@ -107,7 +108,7 @@ while (t<4):
     k83 = -gamma21*ro213 - 1.0*I*(-dp*ro213 - 0.5*oc*ro113 + 0.5*oc*ro223 - 0.5*op*ro013 - ro213*(dc - dp))
     k93 = -Gamma02*ro223 - Gamma12*ro223 - 1.0*I*(-0.5*oc*ro123 + 0.5*oc*ro213 - 0.5*op*ro023 + 0.5*op*ro203)
 #    ft4 = t + (h/2.)
-    ro004 = ro[0]+(h/2.)*k13
+    ro004 = ro00+(h/2.)*k13
     ro014 = ro01+(h/2.)*k23
     ro024 = ro02+(h/2.)*k33
     ro104 = ro10+(h/2.)*k43
@@ -134,86 +135,34 @@ while (t<4):
     ro12 = ro12 + (h/6.)*(k61+(2.*k62)+(2.*k63)+k64)
     ro20 = ro20 + (h/6.)*(k71+(2.*k72)+(2.*k73)+k74)
     ro21 = ro21 + (h/6.)*(k81+(2.*k82)+(2.*k83)+k84)
-    ro22 = ro22 + (h/6.)*(k91+(2.*k92)+(2.*k93)+k94)    
-    a.append(ro21.imag)
-    f.append(t)
-    
-plt.plot(f,a)
-=======
-    ro002 = ro00+(h/2.)*k11
-    ro012 = ro01+(h/2.)*k21
-    ro102 = ro10+(h/2.)*k31
-    ro112 = ro11+(h/2.)*k41
-    k12 = ro112 - I*(o*ro012/2. - o*ro102/2.)
-    k22 = -ro012 - I*(-d*ro012 + o*ro002/2. - o*ro112/2.)
-    k32 = -ro102 - I*(d*ro102 - o*ro002/2. + o*ro112/2.)
-    k42 = -ro112 - I*(-o*ro012/2. + o*ro102/2.)
-#    ft3 = t + (h/2.)
-    ro003 = ro002+(h/2.)*k12
-    ro013 = ro012+(h/2.)*k22
-    ro103 = ro102+(h/2.)*k32
-    ro113 = ro112+(h/2.)*k42
-    k13 = ro113 - I*(o*ro013/2. - o*ro103/2.)
-    k23 = -ro013 - I*(-d*ro013 + o*ro003/2. - o*ro113/2.)
-    k33 = -ro103 - I*(d*ro103 - o*ro003/2. + o*ro113/2.)
-    k43 = -ro113- I*(-o*ro013/2. + o*ro103/2.)
-#    ft4 = t + (h/2.)
-    ro004 = ro003+(h/2.)*k13
-    ro014 = ro013+(h/2.)*k23
-    ro104 = ro103+(h/2.)*k33
-    ro114= ro113+(h/2.)*k43
-    k14 = ro114 - I*(o*ro014/2. - o*ro104/2.)
-    k24= -ro014- I*(-d*ro014 + o*ro004/2. - o*ro114/2.)
-    k34 = -ro104 - I*(d*ro104 - o*ro004/2. + o*ro114/2.)
-    k44 = -ro114- I*(-o*ro014/2. + o*ro104/2.)
-    t = t + (h/2.)
-    ro00 = ro00 + (h/6.)*(k11+(2.*k12)+(2.*k13)+k14)
-    ro01 = ro01 + (h/6.)*(k21+(2.*k22)+(2.*k23)+k24)
-    ro10 = ro10 + (h/6.)*(k31+(2.*k32)+(2.*k33)+k34)
-    ro11 = ro11 + (h/6.)*(k41+(2.*k42)+(2.*k43)+k44)
+    ro22 = ro22 + (h/6.)*(k91+(2.*k92)+(2.*k93)+k94)
     a.append(ro00.real)
-    c.append(ro01.imag)
-    a1.append(ro01.real)
+    e.append(ro11.real)
+    f.append(ro22.real)
+
+    c.append(ro12.imag)
+    a1.append(ro12.real)
     b.append(t)
 
 #plotovanje 2d
 plt.plot(b,a)
->>>>>>> parent of cd9cfef... resava za sistem od 3 nivoa i 2 lasera
 plt.title("Realni deo")
 plt.xlabel("t")
 plt.ylabel("rho00")
 plt.savefig('ro00re.png')
 plt.show()
-<<<<<<< HEAD
-#plt.plot(b,e)
-#plt.title("Realni deo")
-#plt.xlabel("t")
-#plt.ylabel("rho11")
-#plt.savefig('ro11re.png')
-#plt.show()
-#plt.plot(b,f)
-#plt.title("Realni deo")
-#plt.xlabel("t")
-#plt.ylabel("rho22")
-#plt.savefig('ro22re.png')
-#plt.show()
-# 
-#plt.plot(b,a)
-#plt.title("Realni deo")
-#plt.xlabel("t")
-#plt.ylabel("rho21")
-#plt.savefig('ro21re.png')
-#plt.show()
-#   
-#plt.plot(b,c)
-#plt.title("Imaginarni deo")
-#plt.xlabel("t")
-#plt.ylabel("rho21")
-#plt.savefig('ro21im.png')
-#
-#plt.show()
-#
-=======
+plt.plot(b,e)
+plt.title("Realni deo")
+plt.xlabel("t")
+plt.ylabel("rho11")
+plt.savefig('ro00re.png')
+plt.show()
+plt.plot(b,f)
+plt.title("Realni deo")
+plt.xlabel("t")
+plt.ylabel("rho22")
+plt.savefig('ro00re.png')
+plt.show()
  
 plt.plot(b,a)
 plt.title("Realni deo")
@@ -229,5 +178,3 @@ plt.ylabel("rho01")
 plt.savefig('ro01im.png')
 
 plt.show()
-
->>>>>>> parent of cd9cfef... resava za sistem od 3 nivoa i 2 lasera
