46326c87f8c01358eee4fae7227dd6e03d1397ed
nynazdravkovic <nyna.zdravkovic@gmail.com>
2019-07-26 00:36:25 +0200

    Dobijamo grafik koji lici ali nema smisla :)

--- ft2jne.py
+++ ft2jne.py
@@ -11,47 +11,31 @@ import scipy
 import scipy.integrate as integrate
 from scipy.fftpack import fft, ifft
 
-#za resavanje simbolicki 
-#import sympy
-#from sympy import *
-#from sympy.solvers.solvers import solve_linear_system
-#dc=sympy.Symbol('dc')
-#dp=sympy.Symbol('dp')
-#oc=sympy.Symbol('oc')
-#op=sympy.Symbol('op')
-#ro00=sympy.Symbol('ro00')
-#ro01=sympy.Symbol('ro01')
-#ro02=sympy.Symbol('ro02')
-#gamma01=sympy.Symbol('gamma01')
-#gamma02=sympy.Symbol('gamma02')
-#I = sympy.I
-#za direktno resavanje
-dc=0.
-dp=0.
+dc=0.1
+dp=0.1
 oc=0.5
 op=0.5
-W = 0.1
+W = 1
 gamma01=1.
 gamma02=1.
-#w = 1.
+
 I = complex(0,1)
 n=100
-omega = np.linspace(0,100,100)
 c=1
-udaljenost = []
-vreme = []
-E = np.empty((100,100))
+udaljenost = np.linspace(0,10,n,dtype = 'int16')
+vreme = np.linspace(-5,5,n,dtype = 'int16')
+E = np.empty((n,n))
 epsilon = 1
-#jednacina = sympy.solve([I*w*ro01-I*0.5*oc*ro02+I*0.5*op+ro01*(-gamma01+I*(dc-dp)),I*w*ro02-I*0.5*oc*ro01-I*0.5*op+ro02*(-gamma02 - I*dp)],[ro01,ro02])
-#print(jednacina[ro01])
+
+#vraca ro02 sa tildom
 def f (w):
     sistem = [[I*w+(-gamma01+I*(dc-dp)),-I*0.5*oc],[-I*0.5*oc,I*w+(-gamma02 - I*dp)]]
     resenje = [-I*0.5*op,I*0.5*op]
     jednacina = np.linalg.solve(sistem,resenje)
 #    jednacina1 = jednacina[0]/(I*op)
     jednacina2 = jednacina[1]/(I*op)
-    return(jednacina2)
-print()
+    return(np.conj(jednacina2))
+
 #funkcija koja racuna povrsinu ispod funkcije za odvojei imaginarni i realni deo
 def integral(func,a,b):
     def realnaFja(x):
@@ -71,47 +55,30 @@ def gaus(time,W):
         niz.append(a)
     a = fft(niz)
     return(a)
+
 #Ep0 je niz koji se odmah racuna, to je gaus koji je prethodni prosao kroz fft 
-def ispodIntegrala(w, Ep0,t, z):
-    return(Ep0[t]*np.exp(-I*w*(t-z/c)-np.conj(f(w))*z))
+#da bi bio gaus treba da t ide u neg vrednosti 
+#k je mesto u nizu a t je vreme zato sto je vreme i negaivno a niz je izracunat tim vremenom pre
+def ispodIntegrala(w, Ep0,t,k, z):
+    return(Ep0[k]*np.exp(-I*w*(t-z/c)-f(w)*z))
+    
 def resavanje(distance,time,W):
-    matricaResenja = np.zeros((time,distance),dtype = 'complex')
-    for  i in range(time):
-        t = i
-        Ep0 = gaus(time,W)
-        for j in range(distance):
-            z = j
-            Ep = integral(lambda w: ispodIntegrala(w,Ep0,t,z),0,np.inf)
-            matricaResenja[i][j] = Ep[0]
+    matricaResenja = np.zeros((len(time),len(distance)),dtype = 'complex')
+    Ep0 = gaus(len(time),W)
+    for  i in range(len(time)):
+        t = time[i]
+        for j in range(len(distance)):
+            z = distance[j]
+            Ep = integral(lambda w: ispodIntegrala(w,Ep0,t,i,z),-np.inf,np.inf)
+            matricaResenja[i][j] = 1/(2*np.pi)*Ep[0]
     return(matricaResenja)
    
-
-#def gaus(time,W):
-#    a = epsilon*np.exp(-2*np.log(time**2/W**2))    
-#    return(a)
-##Ep0 je niz koji se odmah racuna, to je gaus koji je prethodni prosao kroz fft 
-#def ispodIntegrala(w,E0,t,z):
-#    return(E0*np.exp(-I*w*(t-z/c)-np.conj(f(w))*z))
-#def resavanje(distance,time,W):
-#   matricaResenja = np.zeros((time,distance),dtype = 'complex')
-#   matricaGreski = np.zeros((time,distance),dtype = 'complex')
-#   for  i in range(time):
-#       t = i
-#       vreme.append(t)
-#       udaljenost.append(t)
-#       Ep0 = gaus(time,W)
-#       for j in range(distance):
-#           z = j
-#           Ep = integral(lambda w: ispodIntegrala(w,Ep0,t,z),-np.inf,np.inf)
-#           matricaResenja[i][j] = Ep[0]
-#           matricaGreski[i][j] = Ep[1]
-#   return(matricaResenja,matricaGreski)
-
-polje = resavanje(n,n,W)
-poljeGreske = resavanje(n,n,W)
+polje = resavanje(vreme,udaljenost,W)
 #np.savetxt('Ep1.csv',polje, delimiter=',')
-T,D = np.meshgrid(vreme,udaljenost)
-plt.contourf(polje)
-plt.show()
 
+T = np.meshgrid(vreme,udaljenost)
+plt.contourf(T[0],T[1],polje)
+plt.colorbar()
+plt.show()
+#
 
