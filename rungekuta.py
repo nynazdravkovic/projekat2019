069da48e674ff53d34367604c0d2841cc4125a11
nynazdravkovic <nyna.zdravkovic@gmail.com>
2019-07-28 11:55:29 +0200

    sredjene su jednacine i poboljsani pocetni uslovi ali idalje ne daje dobre rezultate

--- rungekuta.py
+++ rungekuta.py
@@ -20,36 +20,50 @@ gamma10=1.
 gamma12=1.
 gamma20=1.
 gamma21=1.
-
-ro00 = 1.
-ro01 = 0.
-ro02 = 0.
-ro10 = 0.
-ro11 = 0.
-ro12 = 0.
-ro20 = 0.
-ro21 = 0.
-ro22 = 0.
-
 op = 0.5
 oc = 0.5
 dp = 0.
 dc = 0.
-oc1 = 0.1
-oc2 = 0.1
+oc1 = 5.
+oc2 = 5.
 dc1 = 0.
 dc2 = 0.
 
 I = complex(0,1)
-pocetniUslovi = [1.,0.,0.,0.,0.,0.,0.,0.,0.]
+
 matricaGustineIm = []
 matricaGustineRe = []
 t = 0.
 h = 0.02
 ro00re = []
 time = []
+nultiStepen=[[1,0,0,0,1,0,0,0,1],
+             [-I*oc1/2,-gamma01+dc1,-I*oc2/2,0,I*oc1/2,0,0,0,0],
+             [0,-I*oc2/2,-gamma02-I*dp,0,0,I*oc1/2,0,0,0],
+             [I*oc1/2,0,0,-gamma10-I*dc1,-I*oc1/2,0,I*oc2/2,0,0],
+             [0,I*oc1/2,0,-I*oc1/2,-Gamma01,-I*oc2/2,0,I*oc2/2,Gamma12],
+             [0,0,I*oc1/2,0,-I*oc2/2,-gamma12-I*(dc1+dp),0,0,I*oc2/2],
+             [0,0,0,I*oc2/2,0,0,-gamma20+I*dp,-I*oc1/2,0],
+             [0,0,0,0,I*oc2/2,0,-I*oc1/2,-gamma21+I*(dc1+dp),-I*oc2/2],
+             [0,0,0,0,0,I*oc2/2,0,-I*oc2/2,-Gamma02-Gamma12]]
+
+nule = np.zeros(9)
+nule[0] = 1
+pocetniUslovi = np.linalg.solve(nultiStepen,nule)
+
+ro00 = pocetniUslovi[0]
+ro01 = pocetniUslovi[1]
+ro02 = pocetniUslovi[2]
+ro10 = pocetniUslovi[3]
+ro11 = pocetniUslovi[4]
+ro12 = pocetniUslovi[5]
+ro20 = pocetniUslovi[6]
+ro21 = pocetniUslovi[7]
+ro22 = pocetniUslovi[8]
 #ako se koristi ova funkcija koriste se master jednacine za sistem sa 3 nivoa 
 # i 3lasera
+#pocetniUslovi = np.zeros(9,dtype = 'complex')
+#pocetniUslovi[0] = 1
 #def mnozenjeK(ro):
 #    k1 = Gamma01*ro[4]+ Gamma02*ro[8] - 1.0*I*(0.5*op*ro[2] - 0.5*op*ro[6])
 #    k2 = -gamma01*ro[1] - 1.0*I*(0.5*oc*ro[2] - 0.5*op*ro[7] - ro[1]*(dc - dp))
@@ -62,17 +76,17 @@ time = []
 #    k9 = -Gamma02*ro[8] - Gamma12*ro[8] - 1.0*I*(-0.5*oc*ro[5] + 0.5*oc*ro[7] - 0.5*op*ro[2] + 0.5*op*ro[6])
 #    return(k1,k2,k3,k4,k5,k6,k7,k8,k9)
 #ako se koristi ova funkcija koriste se master jednacine za sistem sa 3 nivao i 
-# 3 lasera 
+## 3 lasera 
 def mnozenjeK(ro):        
-    f1 = Gamma01*ro[4] + Gamma02*ro[8] - 1.0*I*(0.5*oc1*ro[1] - 0.5*oc1*ro[3])
-    f2 = -gamma01*ro[1] - 1.0*I*(-dc1*ro[1] + 0.5*oc1*ro[0] - 0.5*oc1*ro[4] + 0.5*oc2*ro[2])
-    f3 = -gamma02*ro[2] - 1.0*I*(dp*ro[2] - 0.5*oc1*ro[5] + 0.5*oc2*ro[1])
-    f4 = -gamma10*ro[3] - 1.0*I*(dc1*ro[3] - 0.5*oc1*ro[0] + 0.5*oc1*ro[4] - 0.5*oc2*ro[6])
+    f1 = Gamma01*ro[4] + Gamma02*ro[8] - 1.0*I*(0.5*oc1*ro[1] - 0.5*oc1*ro[3] + 0.5*op*ro[2] - 0.5*op*ro[6])
+    f2 = -gamma01*ro[1] - 1.0*I*(-dc1*ro[1] + 0.5*oc1*ro[0] - 0.5*oc1*ro[4] + 0.5*oc2*ro[2] - 0.5*op*ro[7])
+    f3 = -gamma02*ro[2] - 1.0*I*(dp*ro[2] - 0.5*oc1*ro[5] + 0.5*oc2*ro[1] + 0.5*op*ro[0] - 0.5*op*ro[8])
+    f4 = -gamma10*ro[3] - 1.0*I*(dc1*ro[3] - 0.5*oc1*ro[0] + 0.5*oc1*ro[4] - 0.5*oc2*ro[6] + 0.5*op*ro[5])
     f5 = -Gamma01*ro[4] + Gamma12*ro[8] - 1.0*I*(-0.5*oc1*ro[1] + 0.5*oc1*ro[3] + 0.5*oc2*ro[5] - 0.5*oc2*ro[7])
-    f6 = -gamma12*ro[5] - 1.0*I*(dc1*ro[6] + dp*ro[5] - 0.5*oc1*ro[2] + 0.5*oc2*ro[4] - 0.5*oc2*ro[8])
-    f7 = -gamma20*ro[6] - 1.0*I*(-dp*ro[6] + 0.5*oc1*ro[7] - 0.5*oc2*ro[3])
-    f8 = -gamma21*ro[7] - 1.0*I*(-dc1*ro[7] - dp*ro[7] + 0.5*oc1*ro[6] - 0.5*oc2*ro[4] + 0.5*oc2*ro[8])
-    f9 = -Gamma02*ro[2] - Gamma12*ro[8] - 1.0*I*(-0.5*oc2*ro[5] + 0.5*oc2*ro[7])
+    f6 = -gamma12*ro[5] - 1.0*I*(dc1*ro[5] + dp*ro[5] - 0.5*oc1*ro[2] + 0.5*oc2*ro[4] - 0.5*oc2*ro[8] + 0.5*op*ro[3])
+    f7 = -gamma20*ro[6] - 1.0*I*(-dp*ro[6] + 0.5*oc1*ro[7] - 0.5*oc2*ro[3] - 0.5*op*ro[0] + 0.5*op*ro[8])
+    f8 = -gamma21*ro[7] - 1.0*I*(-dc1*ro[7] - dp*ro[7] + 0.5*oc1*ro[6] - 0.5*oc2*ro[4] + 0.5*oc2*ro[8] - 0.5*op*ro[1])
+    f9 = -Gamma02*ro[8] - Gamma12*ro[8] - 1.0*I*(-0.5*oc2*ro[5] + 0.5*oc2*ro[7] - 0.5*op*ro[2] + 0.5*op*ro[6])
     return(f1,f2,f3,f4,f5,f6,f7,f8,f9)
 def dodavanjeRo(k,ro):
     ro00 = ro[0]+(h/2.)+k[0]
